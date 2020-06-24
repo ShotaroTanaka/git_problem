8312605cff416cc2699bc7450bdea4deede23acc
ShotaroTanaka <shotaro0405@icloud.com>
2020-06-24 23:04:17 +0900

    create calculate.cpp

---
+++ calculation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    int a, b;
+
+    std::cout << "a=";
+    std::cin >> a; 
+    std::cout << "b="; 
+    std::cin >> b;
+
+    std::cout << "a+b=" << a+b << std::endl;
+
+}
